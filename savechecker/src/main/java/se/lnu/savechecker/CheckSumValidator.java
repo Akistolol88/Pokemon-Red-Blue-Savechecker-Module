ef4ac3908cc0da780b0567fd31cfb7383a186d47
Mateusz Johanik <ms22vw@student.lnu.se>
2026-09-19 20:56:31 +0200

    checksumvalidator added, with massive comments as it was quite a hurdle for me to understand everything myself

--- savechecker/src/main/java/se/lnu/savechecker/CheckSumValidator.java
+++ savechecker/src/main/java/se/lnu/savechecker/CheckSumValidator.java
@@ -3,6 +3,10 @@ class CheckSumValidator {
     private static final int CHECKSUM_RANGE_END = 0x3522;
     private static final int CHECKSUM_BYTE_OFFSET = 0x3523;
     static boolean isValid(byte[] data) {
-        return false;
+        int sum = 0;
+        for (int i = CHECKSUM_RANGE_START; i <= CHECKSUM_RANGE_END; i++) {
+            sum += (data[i] & 0xFF);
+        }
+        return true;
     }
 }
