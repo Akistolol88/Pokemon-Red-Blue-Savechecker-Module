25988441c4681ea334c9f477cec0a90425aa3c17
Mateusz Johanik <ms22vw@student.lnu.se>
2026-09-19 19:53:35 +0200

    stub for Checksum Validator

---
+++ savechecker/src/main/java/se/lnu/savechecker/CheckSumValidator.java
@@ -0,0 +1,8 @@
+class CheckSumValidator {
+    private static final int CHECKSUM_RANGE_START = 0x2598;
+    private static final int CHECKSUM_RANGE_END = 0x3522;
+    private static final int CHECKSUM_BYTE_OFFSET = 0x3523;
+    static boolean isValid(byte[] data) {
+        return false;
+    }
+}
