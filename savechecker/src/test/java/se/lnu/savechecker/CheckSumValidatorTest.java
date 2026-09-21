cc548df202d3787bd7084235077513d84efef46c
Mateusz Johanik <ms22vw@student.lnu.se>
2026-09-21 16:17:53 +0200

    updated with a helper function for the test class

--- savechecker/src/test/java/se/lnu/savechecker/CheckSumValidatorTest.java
+++ savechecker/src/test/java/se/lnu/savechecker/CheckSumValidatorTest.java
@@ -10,19 +10,26 @@ import java.util.Arrays;
 class CheckSumValidatorTest {
 
     private static final int FIRST_CHECKSUMMED_BYTE = 0x2598;
+    private static final int CHECKSUMMD_BYTE_OFFSET = 0x3523;
 
+    private byte[] loadSave(String resourcePath) throws Exception {
+        Path saveFile = Path.of(getClass().getResource(resourcePath).toURI());
+        byte[] data = Files.readAllBytes(saveFile);
+        return data;
+    }
     @Test
     void isValidPokemonGen1Save() throws Exception {
-        Path saveFile = Path.of(getClass().getResource("/saves/Red/Pokemon Red (UE) [S][!].sav").toURI());
-        byte[] data = Files.readAllBytes(saveFile);
-        assertTrue(CheckSumValidator.isValid(data));
+        assertTrue(CheckSumValidator.isValid(loadSave("/saves/Red/Pokemon Red (UE) [S][!].sav")));
     }
     @Test
     void rejectsCorruptedData() throws Exception {
-        Path saveFile = Path.of(getClass().getResource("/saves/Yellow/Yellow_Random_01.srm").toURI());
-        byte[] data = Files.readAllBytes(saveFile);
+        byte[] data = loadSave("/saves/Yellow/Yellow_Random_01.srm");
         byte[] copyData = Arrays.copyOf(data, data.length);
         copyData[FIRST_CHECKSUMMED_BYTE] = 96;
         assertFalse(CheckSumValidator.isValid(copyData));
+    }
+    @Test
+    void rejectsCorruptedCheckSum() throws Exception {
+
     }
 }
