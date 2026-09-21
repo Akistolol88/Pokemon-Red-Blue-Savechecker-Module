2e242ec8e94d86efe65f8ab71395c8fdc2712757
Mateusz Johanik <ms22vw@student.lnu.se>
2026-09-21 16:32:23 +0200

    added a test to loop through all savefiles

--- savechecker/src/test/java/se/lnu/savechecker/CheckSumValidatorTest.java
+++ savechecker/src/test/java/se/lnu/savechecker/CheckSumValidatorTest.java
@@ -10,26 +10,36 @@ import java.util.Arrays;
 class CheckSumValidatorTest {
 
     private static final int FIRST_CHECKSUMMED_BYTE = 0x2598;
-    private static final int CHECKSUMMD_BYTE_OFFSET = 0x3523;
+    private static final int STORED_CHECKSUM_BYTE = 0x3523;
+    private static final String[] ALL_SAVES = {
+        "/saves/Red/Pokemon Red (UE) [S][!].sav",
+        "/saves/Yellow/Yellow_Random_01.srm",
+        "/saves/Yellow/Yellow_Randomizer_01.srm",
+        "/saves/Yellow/Yellow_Randomizer_02.srm"
+    };
 
     private byte[] loadSave(String resourcePath) throws Exception {
         Path saveFile = Path.of(getClass().getResource(resourcePath).toURI());
-        byte[] data = Files.readAllBytes(saveFile);
-        return data;
+        return Files.readAllBytes(saveFile);
     }
     @Test
-    void isValidPokemonGen1Save() throws Exception {
-        assertTrue(CheckSumValidator.isValid(loadSave("/saves/Red/Pokemon Red (UE) [S][!].sav")));
+    void isValidForAllFixtureSaves() throws Exception {
+        for (String path : ALL_SAVES) {
+            assertTrue(CheckSumValidator.isValid(loadSave(path)), path);
+        }
     }
     @Test
     void rejectsCorruptedData() throws Exception {
         byte[] data = loadSave("/saves/Yellow/Yellow_Random_01.srm");
         byte[] copyData = Arrays.copyOf(data, data.length);
-        copyData[FIRST_CHECKSUMMED_BYTE] = 96;
+        copyData[STORED_CHECKSUM_BYTE] = 96;
         assertFalse(CheckSumValidator.isValid(copyData));
     }
     @Test
     void rejectsCorruptedCheckSum() throws Exception {
-
+        byte[] data = loadSave("/saves/Yellow/Yellow_Randomizer_01.srm");
+        byte[] copyData = Arrays.copyOf(data, data.length);
+        copyData[STORED_CHECKSUM_BYTE] = 48;
+        assertFalse(CheckSumValidator.isValid(copyData));
     }
 }
