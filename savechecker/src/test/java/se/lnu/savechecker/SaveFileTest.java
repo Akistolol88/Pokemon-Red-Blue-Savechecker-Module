366978a5f0a50c02b16eea392e6a536a86aa87eb
Mateusz Johanik <ms22vw@student.lnu.se>
2026-09-26 12:27:54 +0200

    added rejectsCorruptedCheckSum test to SaveFileTest

--- savechecker/src/test/java/se/lnu/savechecker/SaveFileTest.java
+++ savechecker/src/test/java/se/lnu/savechecker/SaveFileTest.java
@@ -7,8 +7,12 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Arrays;
 
 class SaveFileTest {
+
+    private static final int STORED_CHECKSUM_BYTE = 0x3523;
+
     @Test
     void rejectsWrongSize() {
         assertThrows(InvalidSaveFileException.class, () -> {
@@ -26,5 +30,15 @@ class SaveFileTest {
                 new SaveFile(data);
             }, save.getFileName().toString());
         }
+    }
+    
+    @Test
+    void rejectsCorruptedCheckSum() throws Exception {
+        byte[] data = SaveFixtures.load("/saves/Yellow/Yellow_Randomizer_02.srm");
+        byte[] copyData = Arrays.copyOf(data, data.length);
+        copyData[STORED_CHECKSUM_BYTE] = (byte) (copyData[STORED_CHECKSUM_BYTE] + 1);
+        assertThrows(InvalidSaveFileException.class, () -> {
+            new SaveFile(copyData);
+        });
     }
 }
